514a91436b5f44c20a21d8f7dd6e12ac6a9f58fc
sundar <sundarrock1999@gmail.com>
2024-07-27 15:20:01 +0530

    hystrix.stream health check and monitor

--- Monitor/src/main/java/com/hystrix/dasborad/Monitor/MonitorApplication.java
+++ Monitor/src/main/java/com/hystrix/dasborad/Monitor/MonitorApplication.java
@@ -1,17 +1,16 @@
-package com.cloud.apiservice;
+package com.hystrix.dasborad.Monitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
-@EnableEurekaClient
-@EnableHystrix
-public class ApiserviceApplication {
+@EnableHystrixDashboard
+public class MonitorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiserviceApplication.class, args);
+		SpringApplication.run(MonitorApplication.class, args);
 	}
 
 }
---
+++ Monitor/src/test/java/com/hystrix/dasborad/Monitor/MonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hystrix.dasborad.Monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
